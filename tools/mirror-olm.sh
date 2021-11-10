6902345d1270a115632c9fafaa5fe717ae77c614
Juan Manuel Parrilla Madrid <jparrill@redhat.com>
2021-11-10 11:06:56 +0100

    Fixing tests

--- tools/mirror-olm.sh
+++ tools/mirror-olm.sh
@@ -33,11 +33,10 @@ fi
 
 mirror() {
 	# Check for credentials for OPM
-	if [ ! -f ~/.docker/config.json ]
-	then
+	if [ ! -f ~/.docker/config.json ]; then
 		echo "ERROR: missing ~/.docker/config.json config"
-    		exit 1
-  	fi
+		exit 1
+	fi
 
 	# Mirror redhat-operator index image
 	if [ "${RH_OP}" = true ]; then
